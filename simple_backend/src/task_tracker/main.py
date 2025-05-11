b2ea319173e490b544f035adbeef8c235403bae9
Ilya <iliagod1337@gmail.com>
2025-05-11 14:31:42 +0300

    Виртуальное окружение развернуто и сервер запущен

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -17,3 +17,11 @@ def update_task(task_id: int):
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     pass
+
+#INFO:     Started server process [14848]
+#INFO:     Waiting for application startup.
+#INFO:     Application startup complete.
+#INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
+#INFO:     127.0.0.1:62006 - "GET /docs HTTP/1.1" 200 OK
+#INFO:     127.0.0.1:62006 - "GET /openapi.json HTTP/1.1" 200 OK
+#INFO:     127.0.0.1:62007 - "GET /tasks HTTP/1.1" 200 OK
