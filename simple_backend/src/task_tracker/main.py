0f6e56d33026a30f189cbb8ab4d7cb5f95916ba1
Ilya <iliagod1337@gmail.com>
2025-05-12 21:27:50 +0300

    update main for integration with github gist

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,32 +1,33 @@
-from task_tracker import TaskStorage
 from fastapi import FastAPI
+from async_task_tracker import GistStorage
+
 
 app = FastAPI()
-storage = TaskStorage()
+storage = GistStorage()
 
 
 @app.get("/tasks")
-def get_tasks():
-    return storage.get_all_tasks()
+async def get_tasks():
+    return await storage.fetch()
 
 
 @app.post("/tasks")
-def create_task(title: str):
-    new_task = storage.create_new_task(title)
+async def create_task(title: str):
+    new_task = await storage.create_new_task(title)
     return {f'Task is created: {new_task}'}
 
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, title: str, status: int):
-    update_t = storage.update_task(task_id, title, status)
+async def update_task(task_id: int, title: str, status: int):
+    update_t = await storage.update_task(task_id, title, status)
     if update_t is None:
-        return {f'Your task is not exist!'}
+        return {'Your task is not exist!'}
     return {f'Task is updated: {update_t}'}
 
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    deleted = storage.remove_task(task_id)
+async def delete_task(task_id: int):
+    deleted = await storage.remove_task(task_id)
     if deleted is None:
-        return f'Your task is not exist!'
+        return 'Your task is not exist!'
     return {f'Task {deleted} was deleted!'}
